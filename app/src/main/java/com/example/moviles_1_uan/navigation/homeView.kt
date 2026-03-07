acf4e6859b3b56cb9aa49272ef6d54e5fc9b02ab
seidllerjuan <juan.seidlleriest.edu.mx>
2026-03-06 18:54:24 -0600

    marzo 6

---
+++ app/src/main/java/com/example/moviles_1_uan/navigation/homeView.kt
@@ -0,0 +1,28 @@
+package com.example.moviles_1_uan.navigation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+//@Preview(showBackground = true)
+@Composable
+fun HomeView(navegar: NavHostController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Bienvenido")
+        Button(onClick = {
+            navegar.navigate(Report)
+        }) {
+            Text("Comenzar")
+        }
+    }
+}
--- app/src/main/java/com/example/moviles_1_uan/navigation/navmanager.kt
+++ app/src/main/java/com/example/moviles_1_uan/navigation/navmanager.kt
@@ -1,17 +1,38 @@
 package com.example.moviles_1_uan.navigation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import kotlinx.serialization.Serializable
+
+@Serializable
+object Home
+
+@Serializable
+object Report
 
 @Preview(showBackground = true)
 @Composable
-fun NavManager(){
+fun NavManager() {
     val navController = rememberNavController()
-    NavHost(navController, startDestination="inicio"){
+    NavHost(navController, startDestination = Home) {
+        composable<Home> {
+            HomeView(navController)
+        }
 
+        composable<Report> {
+            ReportView(navController)
+        }
     }
 }
+
