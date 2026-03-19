b04a7a52b3c2b123c051131700d3401c77487834
seidllerjuan <juan.seidlleriest.edu.mx>
2026-03-18 18:57:49 -0600

    MARZO 18

--- app/src/main/java/com/example/moviles_1_uan/corutinas/StudentViewModel.kt
+++ app/src/main/java/com/example/moviles_1_uan/corutinas/StudentViewModel.kt
@@ -1,15 +1,54 @@
 package com.example.moviles_1_uan.corutinas
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 import java.lang.Thread.sleep
 
-class StudentViewModel {
+class StudentViewModel: ViewModel() {
     var winner by mutableStateOf("")
+        private set
+
+    var isloading by mutableStateOf(false)
+        private set
+    var lista = mutableListOf<String>()
+        private set
+
+    init {
+        lista = mutableListOf("Carlos","Juan","Pedro")
+
+    }
+
     fun getWinner(){
-        winner = ""
-        sleep(5000)
-        winner = "Juan Perez Perez"
+        isloading = true
+        try {
+            viewModelScope.launch {
+                winner = ""
+                changePerson()
+            }
+        } catch (e: Exception){
+            Log.e("Error background", "Error eb modulo studentViewModel $e")
+        }finally {
+            isloading = false
+
+        }
+    }
+
+    suspend fun changePerson(){
+        withContext(Dispatchers.IO){
+            isloading=true
+            delay(5000)
+            winner = "Juan Perez Perez"
+            isloading = false
+
+        }
     }
 }
--- app/src/main/java/com/example/moviles_1_uan/corutinas/winnerview.kt
+++ app/src/main/java/com/example/moviles_1_uan/corutinas/winnerview.kt
@@ -5,7 +5,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -16,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @SuppressLint("ViewModelConstructorInComposable")
@@ -43,8 +46,14 @@ fun WinnerView(studentsVM: StudentViewModel){
         }) {
             Text("Comenzar")
         }
+        CircularProgressIndicator(modifier = Modifier.size(64.dp))
+
+        if (studentsVM.isloading){
+            Text("Cargando por favor respere")
+        }else {
+            Text("El ganador es... ${studentsVM.winner}")
+        }
     }
 
-    Text("El ganador es... ${studentsVM.winner}")
 
 }
